66548cd5e63b7665f4900185c57f6f80e2800bb3
lananhkosuy111 <thecoolbionam1311a@outlook.com>
2024-05-13 02:11:18 +0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function levelOrder(root) {
-  if (!root) return [];
-  const result = [];
-  const queue = [root];
-  while (queue.length) {
-    const levelSize = queue.length;
-    const currentLevel = [];
-    for (let i = 0; i < levelSize; i++) {
-      const node = queue.shift();
-      currentLevel.push(node.val);
-      if (node.left) queue.push(node.left);
-      if (node.right) queue.push(node.right);
-    }
-    result.push(currentLevel);
-  }
-  return result;
-}
+const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
