cc60359847e201a781925bd57d9b87327d9eb9e1
lananhkosuy111 <thecoolbionam1311a@outlook.com>
2024-05-14 16:52:48 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-console.log(randomNumber);
+function sortedListToBST(head) {
+  if (!head) return null;
+  if (!head.next) return new TreeNode(head.val);
+  let slow = head;
+  let fast = head;
+  let prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const root = new TreeNode(slow.val);
+  prev.next = null;
+  root.left = sortedListToBST(head);
+  root.right = sortedListToBST(slow.next);
+  return root;
+}
